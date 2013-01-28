5d222b3a04900240cfc3c7d53b74da9b831d4814
Aaron Baker <bakis2011@gmail.com>
2013-01-28 13:21:26 -0500

    add comments to player.rb

--- player.rb
+++ player.rb
@@ -1,14 +1,24 @@
 class Player
   attr_reader :players
 
+  # Initializes an empty players array to store the players
   def initialize
     @players = []
   end
 
+  # Creates a new player with given name
+  #
+  # Input: name - name of player
   def new_player(name)
     @players << name
   end
 
+  # This method switches the player whose turn it is.
+  # This switches 1 to 2 or 2 to 1.
+  #
+  # Input: player - whose turn it is
+  #
+  # Output: the opposite player (1 if 2, 2 if 1)
   def next_player
     @players << @players.shift
   end
